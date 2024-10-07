459d25a45c0adda9fae39c874bf963c4aa4566bc
Aykut <kutay-baglan@hotmail.com>
2024-10-07 18:02:43 +0300

    Revert "When we select the bird, the start button is activated"
    This reverts commit fff5a0406fb603f0e8b836d4dfa7c68da3b10c47.

--- Assets/Scripts/StartMenu.cs
+++ Assets/Scripts/StartMenu.cs
@@ -7,7 +7,6 @@ public class StartMenu : MonoBehaviour
 {
     public GameManager gameManager;
     public birdController birdController;
-    public ShopManager shopManager;
 
     public void Start()
     {
@@ -25,18 +24,7 @@ public class StartMenu : MonoBehaviour
     {
         gameManager.scoreRankingPanelGo.SetActive(false);
         gameManager.Shop_Birds.SetActive(false);
+        gameManager.startButtonGo.SetActive(true);
         gameManager.restartButtonGo.SetActive(true);
-        for (int i = 0; i < shopManager.buttons.Length; i++)
-        {
-            if (PlayerPrefs.GetInt("BirdPurchased_" + i) == 1) // Bu acýklanacak!
-            {
-                gameManager.startButtonGo.SetActive(true);
-                return;
-            }
-            else
-            {
-                gameManager.startButtonGo.SetActive(false);
-            }
-        }
     }
 }
