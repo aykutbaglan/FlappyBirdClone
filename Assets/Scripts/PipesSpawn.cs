7a40538324d1c710b798d9ffc8c6dd45456910fc
Aykut <kutay-baglan@hotmail.com>
2025-01-02 16:29:34 +0300

    variable name changed

--- Assets/Scripts/PipesSpawn.cs
+++ Assets/Scripts/PipesSpawn.cs
@@ -4,24 +4,26 @@ public class PipesSpawn : MonoBehaviour
 {
     [SerializeField] private GameObject pipes;
     [SerializeField] private Transform pipesSpawner;
-    [SerializeField] private float spawnspeed = 10f;
+    [SerializeField] private float moveSpeed = 3f;
     [SerializeField] private float upHeight = 1f;
     [SerializeField] private float downHeight = -5f;
     [SerializeField] private float xPos = 25f;
+    [SerializeField] private float spawnRate = 2f;
+    [SerializeField] private float despawnTime = 9f;
 
     private void Start()
     {
-        Invoke(nameof(Spawn), 1f);
+        Spawn();
     }
     private void Update()
     {
-        transform.Translate(Vector3.right * spawnspeed * Time.deltaTime);
+        transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
     }
-    public void Spawn()
+    private void Spawn()
     {
         Vector3 pipesSpawner = new Vector3(transform.position.x + xPos,UnityEngine.Random.Range(downHeight,upHeight));
         GameObject newpipe = Instantiate(pipes,pipesSpawner,Quaternion.identity);
-        Destroy(newpipe,7f);
-        Invoke(nameof(Spawn),2f);
+        Destroy(newpipe,despawnTime);
+        Invoke(nameof(Spawn),spawnRate);
     }
 }
