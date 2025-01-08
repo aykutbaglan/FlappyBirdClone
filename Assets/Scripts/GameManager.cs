1d24e81019149035a061b0744ba77f99b6a92bdc
Aykut <kutay-baglan@hotmail.com>
2025-01-08 16:04:49 +0300

    (startmenu) change: text position

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -58,7 +58,7 @@ public class GameManager : MonoBehaviour
         GamePause();
         _canvasMainMenu.OnEnter();
     }
-    public void QuitGame()
+    private void QuitGame()
     {
         Application.Quit();
     }
